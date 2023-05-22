1e8449fa16ba528c62fe5b209c0dc39d1df83c87
Bruno_Lanziloti47 <122484759+BrunoHangar@users.noreply.github.com>
2023-05-22 17:30:24 -0300

    String Matching
    Algoritmos que fazem a procura de uma substring dentro de uma string para verificar se há ocorrência da dada sequência de caracteres.

---
+++ Contest 6882/A.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 100000
+
+// Função para converter a string para letras minúsculas
+void Minusculo(char *string) {
+    while (*string) {
+        *string = tolower(*string);
+        string++;
+    }
+}
+
+//Faz uso da função strstr dentro da biblioteca string.h, que tem sua utilização baseada no algoritmo
+// de Boyer-Moore, onde ela busca subtrings dentro de uma string.
+int main() {
+    char string[MAX], *resultado;
+
+    fgets(string, MAX, stdin);
+    string[strcspn(string, "\n")] = '\0';
+
+    // Converte a string para letras minúsculas
+    Minusculo(string);
+
+    // Verificar a presença da palavra "zelda" na string
+    resultado = strstr(string, "zelda");
+
+    if (resultado != NULL) {
+        printf("Link Bolado\n");
+    } else {
+        printf("Link Tranquilo\n");
+    }
+
+    return 0;
+}
